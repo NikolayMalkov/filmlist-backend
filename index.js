79884d929619d6a496049cbcc54e5b206fefcb05
Nikolay Malkov <fcttmk@yandex.ru>
2022-02-23 16:57:36 +0700

    postgres

--- index.js
+++ index.js
@@ -1,37 +1,55 @@
 const express = require('express');
 const config = require('config');
-const mongoose = require('mongoose');
-const routes = require('./routes/routes');
-const cors = require('cors');
+// const routes = require('./routes/routes');
+// const cors = require('cors');
 
 const app = express()
 
 
 const PORT = config.get('port') || 5000
 
-// Коннект к монго
-async function start() {
-    try {
-        await mongoose.connect(config.get('mongoUri'), {
-
-        })
-    }
-    catch (e) {
-        console.log(e.message)
-        process.exit(1)
-    }
-}
-start();
 
 // Корс обоссаный
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
     next();
   });
 // Хрень чтобы распарсить в жсон
 app.use(express.json())
-app.use('/', routes)
-app.use('/films', routes)
+
+// Получить список фильмов
+app.get('/', (req, res) => {
+    Film.getFilms()
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+  
+  app.post('/films', (req, res) => {
+    Film.createFilm(req.body)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+  
+  app.delete('/films/:id', (req, res) => {
+    Film.deleteFilm(req.params.id)
+    .then(response => {
+      res.status(200).send(response);
+    })
+    .catch(error => {
+      res.status(500).send(error);
+    })
+  })
+
+
 
 app.listen(PORT, () => console.log(`server has been started on ${PORT}`))
--- models/Film.js
+++ models/Film.js
@@ -1,7 +1,48 @@
-const {Schema, model} = require('mongoose');
 
-const schema = new Schema({
-    title: { type: String, unique: true }
-})
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'sysdba',
+  host: 'postgresdb',
+  database: 'films',
+  password: 'root',
+  port: 5432,
+});
 
-module.exports = model('Film', schema)
+const getFilms = () => {
+    return new Promise(function(resolve, reject) {
+      pool.query('SELECT * FROM films', (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(results.rows);
+      })
+    }) 
+  }
+  const createFilm = (body) => {
+    return new Promise(function(resolve, reject) {
+      const { title } = body
+      pool.query('INSERT INTO films (title) VALUES ($1) RETURNING *', [title], (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(`A new film has been added: ${results.rows[0]}`)
+      })
+    })
+  }
+  const deleteFilm = () => {
+    return new Promise(function(resolve, reject) {
+      const id = parseInt(request.params.id)
+      pool.query('DELETE FROM films WHERE id = $1', [id], (error, results) => {
+        if (error) {
+          reject(error)
+        }
+        resolve(`Film deleted with ID: ${id}`)
+      })
+    })
+  }
+  
+  module.exports = {
+    getFilms,
+    createFilm,
+    deleteFilm,
+  }
