21542e11e7df562cc32eea25ed8391debb64879d
Nikolay Malkov <fcttmk@yandex.ru>
2022-02-25 16:00:43 +0700

    jopa

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const app = express()
 const PORT = config.get('port') || 5000
 
 // Корс обоссаный
+// рот пер
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
